ff023d08b042f1804d11fb44837ab03baec294e5
Goofy <goofy_clunctia@hotmail.com>
2018-08-13 17:09:04 +0900

    add more function to splitandsampling

--- Effort_Estimate/src/HoldOutAndCrossValidation.java
+++ Effort_Estimate/src/HoldOutAndCrossValidation.java
@@ -13,17 +13,42 @@ import weka.filters.unsupervised.attribute.RemoveByName;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class HoldOutAndCrossValidation {
-/*
- * Your project is to answer the following question:
- * Which of 10-fold CV or Hold-out is better to evaluate (and compare) effort estimation models.
- * Fig.3 in the paper shows what functions should be implemented.
- * The white boxes in the figure correspond to functions.
- * "Model Construction" corresponds to building a Linear Regression model.
- * "Performance Measurement" corresponds to calculating a mean absolute error.
- * "Data Preparation for Performance Evaluation" corresponds to the techniques such as 10-fold CV and Hold-out.
- * Please read the paper carefully, and try to implement the remained boxes.
- */
+	/*
+	 * Your project is to answer the following question:
+	 * Which of 10-fold CV or Hold-out is better to evaluate (and compare) effort estimation models.
+	 * Fig.3 in the paper shows what functions should be implemented.
+	 * The white boxes in the figure correspond to functions.
+	 * "Model Construction" corresponds to building a Linear Regression model.
+	 * "Performance Measurement" corresponds to calculating a mean absolute error.
+	 * "Data Preparation for Performance Evaluation" corresponds to the techniques such as 10-fold CV and Hold-out.
+	 * Please read the paper carefully, and try to implement the remained boxes.
+	 */
+	// int[] Array1 = new int[] {1,652,5,15,385,4,55,666,13};
+	// int[] Array2 = new int[] {2,4658,9,55,-588,10,1083,17};
+	// contains = false;
+	// List<Integer> results = new ArrayList<Integer>();
+	// System.out.println(Array1.length);
+	// System.out.println(Array2.length);
+	//
+	// for(int i=0; i<Array1.length; i++) {
+	//     for(int j=0; j<Array2.length; j++) {
+	//         if(Array1[i]==Array2[j]) {
+	//             contains = true;
+	//             break;
+	//         }
+	//     }
+	//     if(!contains) {
+	//         results.add(Array1[i]);
+	//     }
+	//     else{
+	//         contains = false;
+	//     }
+	// }
+	//
+	// System.out.println(results);
 }
--- Effort_Estimate/src/SplitAndSampling.java
+++ Effort_Estimate/src/SplitAndSampling.java
@@ -1,5 +1,7 @@
 import java.util.Random;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import weka.attributeSelection.HoldOutSubsetEvaluator;
 import weka.classifiers.Classifier;
@@ -13,8 +15,10 @@ import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.RemoveByName;
 import weka.filters.unsupervised.instance.Randomize;
 import weka.filters.unsupervised.instance.RemovePercentage;
+import weka.filters.unsupervised.instance.Resample;
 
 import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.math.*;
 
 public class SplitAndSampling {
 	static String fileLocation;
@@ -25,11 +29,142 @@ public class SplitAndSampling {
 	static DataSource source;
 	static double[] resultAvg;
 	
-	public SplitAndSampling() {
-		fileLocation = "./data/miyazaki94.arff";
+	public static void main(String[] args) {
+		try {
+			fileLocation = "./data/miyazaki94.arff";
+			source = new DataSource(fileLocation);
+			Instances dataset_ID = source.getDataSet();
+			RemoveByName rbName = new RemoveByName();
+			rbName.setExpression("ID");
+			rbName.setInputFormat(dataset_ID);
+			dataset = Filter.useFilter(dataset_ID, rbName);
+			dataset.setClassIndex(dataset.numAttributes()-1);
+			
+			System.out.println("Size of all data: " + dataset.size() + "\n");
+			
+			Instances splited[] = splitIntoThree(dataset);
+			
+			Randomize randomize = new Randomize();
+			rand = new Random();
+			randomize.setRandomSeed(rand.nextInt());
+			randomize.setInputFormat(dataset);
+			
+			Instances small = splited[0];
+			Instances medium = splited[1];
+			Instances large = splited[2];
+			
+			Instances smallResample, mediumResample, largeResample;
+			
+			double sizePercentage = 70;
+			
+			Resample re = new Resample();
+			
+			re.setInputFormat(small);
+			re.setRandomSeed(rand.nextInt());
+			re.setNoReplacement(true);
+			re.setSampleSizePercent(sizePercentage);
+			smallResample = Filter.useFilter(small, re);
+			
+			re.setInputFormat(medium);
+			re.setRandomSeed(rand.nextInt());
+			re.setNoReplacement(true);
+			re.setSampleSizePercent(sizePercentage);
+			mediumResample = Filter.useFilter(medium, re);
+			
+			re.setInputFormat(large);
+			re.setRandomSeed(rand.nextInt());
+			re.setNoReplacement(true);
+			re.setSampleSizePercent(sizePercentage);
+			largeResample = Filter.useFilter(large, re);
+			
+			//Extract the unseen data that not in the random sampling data set
+			
+			List<Instance> smallRemain = findRemain(small, smallResample);
+			List<Instance> mediumRemain = findRemain(medium, mediumResample);
+			List<Instance> largeRemain = findRemain(large, largeResample);
+			
+			
+			
+		}catch (Exception e) {
+			System.out.println("Error exception : " + e);
+		}
 		
 	}
 	
-
+	public static List<Instance> findRemain(Instances data, Instances sampledData){
+		List<Instance> remain = new ArrayList<Instance>();
+		boolean contains = false;
+		
+		for(int i = 0 ; i < data.size(); i++){
+			for(int j = 0 ; j < sampledData.size() ; j++) {
+				if(data.get(i).toString().equals(sampledData.get(j).toString())) {
+					contains = true;
+					break;
+				}
+			}
+			if(!contains) remain.add(data.get(i));
+			else contains = false;
+		}
+		
+		return remain;
+	}
 	
+	public static void printInstances(Instances data) {
+		for(int i = 0 ; i < data.size() ; i++) {
+			System.out.println(data.get(i));
+		}
+		System.out.println("Size of Data : " + data.size());
+	}
+	
+	
+	public static Instances[] split(Instances data, double p) throws Exception {
+		Randomize rand = new Randomize();
+		Random r = new Random();
+		rand.setRandomSeed(r.nextInt());
+		rand.setInputFormat(data);
+		data = Filter.useFilter(data, rand);
+		
+		RemovePercentage rp = new RemovePercentage();
+		rp.setInputFormat(data);
+		rp.setPercentage(p);
+		Instances train = Filter.useFilter(data, rp);
+		
+		rp = new RemovePercentage();
+		rp.setInputFormat(data);
+		rp.setPercentage(p);
+		rp.setInvertSelection(true);
+		Instances test = Filter.useFilter(data, rp);
+		
+		return new Instances[] {train, test};
+	}
+	
+	public static Instances[] splitIntoThree (Instances data) throws Exception {
+		Instances small, medium, large, tmp;
+		double sixty = 66;
+		double fifty = 50;
+		
+		//sort the data by the last attribute of the instances (Effort)
+		data.sort(data.numAttributes()-1);
+		
+		RemovePercentage rp = new RemovePercentage();
+		rp.setInputFormat(data);
+		rp.setPercentage(sixty);
+		tmp = Filter.useFilter(data, rp);
+		
+		rp.setInputFormat(data);
+		rp.setPercentage(sixty);
+		rp.setInvertSelection(true);
+		large = Filter.useFilter(data, rp);		
+		
+		rp.setInputFormat(tmp);
+		rp.setPercentage(fifty);
+		small = Filter.useFilter(tmp, rp);
+		
+		rp.setInputFormat(tmp);
+		rp.setPercentage(fifty);
+		rp.setInvertSelection(true);
+		medium = Filter.useFilter(tmp, rp);
+				
+		return new Instances[] {small, medium, large};
+	}
 }
